05ab6ac53ca368f0c5f0cceb86a3767e4ebc9e44
aman <00amanwalia@gmail.com>
2022-08-21 10:05:38 +0530

    react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
-
+import {Routes , Route} from 'react-router-dom'
+import Account from './components/Account';
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import RecoverAccount from './components/RecoverAccount';
+import SignUp from './components/SignUp';
 function App() {
   return (
     <>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/account' element={<Account></Account>}></Route>
+        <Route path='/recoveraccount' element={<RecoverAccount></RecoverAccount>}></Route>
+      </Routes>
+    </div>
     </>
   );
 }
